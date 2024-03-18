85507605ff0a6065c84b36f7db326fe16d9de5a1
abhay <abhaydeep7890@gmail.com>
2024-03-18 21:45:07 +0530

    singly ll done(deletion samajh nhi aya

--- sample.cpp
+++ sample.cpp
@@ -54,12 +54,64 @@ void insertAtStart(Node *&head, int data){
     head = temp_node; // ?
 }
 
-void insertAtEnd(Node *&tail,int data){
+void insertAtEnd(Node *&tail, int data){
     //create new node
     Node *temp_node = new Node(data);
     tail->next = temp_node; // new node(tempnode) ka head point kar dia last node(tail) ke NEXt mein
     tail = temp_node; // ?
 }
+
+void insertAtPosition(Node *&head, Node *&tail,int position, int data){
+    //EDGE CASE (position=1)
+    if (position==1)
+    {
+        insertAtStart(head,data);
+        return; // ?
+    }
+    
+    // first node
+    Node *temp = head; // temp is a pointer which will travel to specific location to add node
+    int cnt=1;
+    //loop to traverse/shift temp to required location
+    while (cnt<position-1)
+    {
+        temp=temp->next;
+        cnt++;
+    }
+
+    //creating a node to add
+    Node *nodeToAdd = new Node(data);
+    nodeToAdd->next = temp->next;
+    temp->next = nodeToAdd;
+
+}
+
+void deleteNode(Node *&head,Node *&tail, int position){
+    // delete first node(special case)
+    if (position==1)
+    {
+        Node *temp = head;
+        head = head->next;
+        //memory free starting node
+        temp->next=NULL;
+        delete temp;
+    }
+    
+    // re evaluate this, samaj nhi aaya
+    Node *prev = NULL;
+    Node *temp = head;
+    int cnt=1;
+    while (cnt<position)
+    {
+        prev = temp;
+        temp = temp->next;
+        cnt++;
+    }
+    prev->next = temp->next;
+    temp->next = NULL;
+    delete temp;    
+}
+
 
 int main(){
 
@@ -72,12 +124,16 @@ int main(){
     Node *node1 = new Node(10); //initial node
     // pointing head of node1 in head
     Node *head = node1; 
-    Node *tail = node1;
-    print(head);
+    Node *tail = node1; // node is single/ only one, so thats why head=tail ie. both are same(coz node is single)
+    // print(head);
     // insertAtStart(head,20);
-    insertAtEnd(tail,69);
-    insertAtEnd(tail,420);
-    print(head);
+     insertAtEnd(tail,420);
+     insertAtEnd(tail,240);
 
+    // print(head);
+    insertAtPosition(head,tail,3,66);
+    print(head);
+    deleteNode(head,tail,1);
+    print(head);
     return 0;
 }
